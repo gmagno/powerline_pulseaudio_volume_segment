c59c8777d231322b37948dad417ad830b71140f7
Goncalo <goncalo.magno@gmail.com>
2021-04-12 12:37:01 -0300

    Fix bump2version config

--- powerline_pulseaudio_volume/__init__.py
+++ powerline_pulseaudio_volume/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Powerline PulseAudio Volume Segment."""
 
 __author__ = """Goncalo Magno"""
-__email__ = 'goncalo.magno@gmail.com'
-__version__ = '0.1.1'
+__email__ = "goncalo.magno@gmail.com"
+__version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/gmagno/powerline_pulseaudio_volume_segment",
-    version="version='0.1.1'",
+    version="0.1.1",
     zip_safe=False,
 )
